35957d1fe9bd890a4e787268ba820734c2c6b706
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-02-07 02:45:24 +0400

    additional infomation button js

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// button for additional information 
+const moreInfo = document.querySelector('.more-info');
+const additionalInfo = document.querySelector('.additional-info');
+
+moreInfo.addEventListener('click',()=>{
+    console.log('holaa');
+  additionalInfo.classList.add('active');
+})
+
