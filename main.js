5bf6643ed2b285aa435fee067db59395b58b8364
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-02-07 14:08:43 +0400

    second page html css

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const moreInfo = document.querySelector('.more-info');
 const additionalInfo = document.querySelector('.additional-info');
 
 moreInfo.addEventListener('click',()=>{
-    console.log('holaa');
+   
   additionalInfo.classList.add('active');
 })
 
