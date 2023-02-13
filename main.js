6a6673b6f0835cea37809be6f40f459928cb13af
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-02-13 19:00:24 +0400

    input validity

--- main.js
+++ main.js
@@ -1,180 +1,258 @@
+// button for additional information
+const moreInfo = document.querySelector(".more-info");
+const goBack = document.querySelectorAll(".back-btn");
+const additionalInfo = document.querySelector(".additional-info");
+const addResume = document.querySelector(".add-resume");
+const storePersonalInfo = localStorage.getItem("personal-info");
+const storeExperience = localStorage.getItem("experience");
+const storeEducation = localStorage.getItem("education");
+const addResumeBio = document.querySelector(".add-resume-bio");
+const resumeImg = document.getElementById("resume-img-output");
 
-// button for additional information 
-const moreInfo = document.querySelector('.more-info');
-const goBack = document.querySelectorAll('.back-btn');
-const additionalInfo = document.querySelector('.additional-info');
-const addResume = document.querySelector('.add-resume');
-const storePersonalInfo = localStorage.getItem('personal-info');
-const storeExperience = localStorage.getItem('experience');
-const storeEducation = localStorage.getItem('education');
-const addResumeBio = document.querySelector('.add-resume-bio');
-const resumeImg = document.getElementById('resume-img-output');
-const uploadImage = document.getElementById('upload-img');
+// const nextBtn = document.getElementById("next-btn");
 
-//ეს ფუნცცია გასატანია ცალკე 
+// function disableNext() {
+//     nextBtn.classList.add('isDisabled')
+// }
+// function enableNext() {
+//     nextBtn.classList.remove('isDisabled')
+// }
+
+//ეს ფუნცცია გასატანია ცალკე
 // moreInfo.addEventListener('click',()=>{
 //   additionalInfo.classList.add('active');
 //   addResume.classList.add('add-active-resume');
 //   addResumeBio.classList.add('add-active-resume')
 // })
 
-
-// const FormPersonalInfo = document.getElementById('form-personal-info');
+//
 // const FormExperience = document.getElementById('form-experience');
 // const FormEducation = document.getElementById('form-education');
 
+// const namePerson = document.getElementById("input-name");
+// const surname = document.getElementById("last-name");
+// const email = document.getElementById("input-mail");
+// const phoneNumber = document.getElementById('input-number');
+// const aboutMe = document.getElementById("input-about-me");
+// const experience = document.getElementById('input-position');
+// const educations = document.getElementById('input-education');
+// const image = document.getElementById('upload-img');
+
+// const namePersonValue = namePerson.value
+// const surnameValue = surname.value
+// const emailValue = email.value
+// const phoneNumberValue = phoneNumber.value
+// const experienceValue = experience.value
+// const eduactionsValue = educations.value
+// const aboutMeValue = aboutMe.value
+// const imageValue = image.value
+
+// console.log(namePersonValue, surnameValue,emailValue, phoneNumberValue,experienceValue,eduactionsValue, aboutMeValue );
 // function submit(){
 //     resForm.addEventListener('submit',(e)=>{
 //         e.preventDefault;
+//         const user = {
+
+//         }
 //     })
 // }
 // submit();
-function getUser(){
-    fetch('https://resume.redberryinternship.ge/api/degrees', {
-        method:'GET'
-    }).then(res=>{
-        console.log(res);
-        return res.json();
-    })
-    .then((data)=>{
-        console.log(data);
-      
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
-}
 
-function postResume(){
-     fetch('https://resume.redberryinternship.ge/api/cvs',{
-        method:'POST',
-        headers: {"Content-type": "application/json"},
-        body: JSON.stringify()
-     })
-     .then((res)=>res.json())
-     .then((data)=>{
-        console.log(data);
-     })
+// function validatePattern() {
+//     const name =  document.getElementById('input-name').value
+//     const reg = /^[ა-ჰ]+$/
+//     if(reg.test(name)){
+//         console.log('ak mxolod kartulia')
+//     }else {
+//         document.getElementById('input-name').value = ''
+//     }
+
+// }
+
+function getUser() {
+  fetch("https://resume.redberryinternship.ge/api/degrees", {
+    method: "GET",
+  })
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+console.log({ resumeImg });
+
+function postResume() {
+  fetch("https://resume.redberryinternship.ge/api/cvs", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
 }
 // postResume();
 
-
 getUser();
 
-function clearStorage(){
-    localStorage.clear();
+function clearStorage() {
+  localStorage.clear();
 }
-function save(key,arr){
+function save(key, arr) {
   localStorage.setItem(key, arr);
 }
 function sendToStorage(arr, func, key) {
-    const myarr = arr
-    func(myarr)
-    let newArr =[]
-    for (let i =0; i < arr.length;  i++){
-        newArr.push(arr[i])
-        newArr.push(';')
-    }
-    newArr.pop()
-    save(key, newArr)
+  const myarr = arr;
+  func(myarr);
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i]);
+    newArr.push(";");
+  }
+  newArr.pop();
+  save(key, newArr);
 }
 
-
-function addInfoInResume(arr){
-    document.getElementById('resume-name').innerHTML = arr[0];
-    document.getElementById('resume-last-name').innerHTML = arr[1];
-    document.getElementById('resume-my-bio').innerHTML = arr[2]; 
-    document.getElementById('resume-mail').innerHTML = arr[3];
-    document.getElementById('resume-number').innerHTML = arr[4];
+function addInfoInResume(arr) {
+  document.getElementById("resume-name").innerHTML = arr[0];
+  document.getElementById("resume-last-name").innerHTML = arr[1];
+  document.getElementById("resume-my-bio").innerHTML = arr[2];
+  document.getElementById("resume-mail").innerHTML = arr[3];
+  document.getElementById("resume-number").innerHTML = arr[4];
+  //   document.getElementById("resume-img-output").innerHTML = arr[5];
 }
-function addExperienceInResumee(arr){
-    document.getElementById('resume-position').innerHTML = arr[0];
-    document.getElementById('resume-work').innerHTML = arr[1];
-    document.getElementById('resume-start-date').innerHTML = arr[2];
-    document.getElementById('resume-end-date').innerHTML =  arr[3];
-    document.getElementById('resume-my-exp').innerHTML = arr[4];
+function addExperienceInResumee(arr) {
+  document.getElementById("resume-position").innerHTML = arr[0];
+  document.getElementById("resume-work").innerHTML = arr[1];
+  document.getElementById("resume-start-date").innerHTML = arr[2];
+  document.getElementById("resume-end-date").innerHTML = arr[3];
+  document.getElementById("resume-my-exp").innerHTML = arr[4];
 }
-function addEducationResumee(arr){
-    document.getElementById('resume-univercity').innerHTML = arr[0];
-    document.getElementById('resume-degree').innerHTML = arr[1];
-    document.getElementById('resume-degree-date').innerHTML = arr[2];
-    document.getElementById('resume-education-desc').innerHTML =  arr[3];
+function addEducationResumee(arr) {
+  document.getElementById("resume-univercity").innerHTML = arr[0];
+  document.getElementById("resume-degree").innerHTML = arr[1];
+  document.getElementById("resume-degree-date").innerHTML = arr[2];
+  document.getElementById("resume-education-desc").innerHTML = arr[3];
 }
-function handleResumee (key,data, func){
+function handleResumee(key, data, func) {
+  if (data) {
     localStorage.getItem(key);
-    const myarr = data.replaceAll(',', '')
-    const splitted = myarr.split(';')
-    func(splitted)
-    return splitted
+    const myarr = data.replaceAll(",", "");
+    const splitted = myarr.split(";");
+    func(splitted);
+    return splitted;
+  }
 }
 
-function getPersonalInfo(){
-    const splitted =  handleResumee('personal-info', storePersonalInfo, addInfoInResume)
-    console.log({splitted})
-    document.getElementById('input-name').value = splitted[0];
-    document.getElementById('last-name').value = splitted[1];
-    document.getElementById('input-about-me').value = splitted[2]; 
-    document.getElementById('input-mail').value = splitted[3];
-    document.getElementById('input-number').value = splitted[4];
+function getPersonalInfo() {
+  const splitted = handleResumee(
+    "personal-info",
+    storePersonalInfo,
+    addInfoInResume
+  );
+  if (splitted !== undefined) {
+    console.log({ splitted });
+    document.getElementById("input-name").value = splitted[0];
+    document.getElementById("last-name").value = splitted[1];
+    document.getElementById("input-about-me").value = splitted[2];
+    document.getElementById("input-mail").value = splitted[3];
+    document.getElementById("input-number").value = splitted[4];
+    // document.getElementById("upload-img").value = splitted[5];
+  }
 }
 
 function savePersonalInfo() {
-    const inputName = document.getElementById('input-name').value;
-    const lastName = document.getElementById('last-name').value;
-    const aboutMe = document.getElementById('input-about-me').value; 
-    const inputMail = document.getElementById('input-mail').value;
-    const inputNumber = document.getElementById('input-number').value;
-    const arr = [inputName, lastName,  aboutMe, inputMail, inputNumber]
-    addInfoInResume(arr)
-    sendToStorage(arr, addInfoInResume, 'personal-info')
+  const inputName = document.getElementById("input-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const aboutMe = document.getElementById("input-about-me").value;
+  const inputMail = document.getElementById("input-mail").value;
+  const inputNumber = document.getElementById("input-number").value;
+  //   const uploadImage = document.getElementById("upload-img").value;
+
+  const arr = [
+    inputName,
+    lastName,
+    aboutMe,
+    inputMail,
+    inputNumber,
+    // uploadImage,
+  ];
+  addInfoInResume(arr);
+  sendToStorage(arr, addInfoInResume, "personal-info");
 }
-function getExperience(){
-    handleResumee('personal-info', storePersonalInfo, addInfoInResume)
-    const splitted =  handleResumee('experience', storeExperience, addExperienceInResumee)
-    console.log({splitted})
-    document.getElementById('input-position').value=splitted[0]
-    document.getElementById('input-work').value=splitted[1]
-    document.getElementById('start-date').value=splitted[2]
-    document.getElementById('end-date').value=splitted[3]
-    document.getElementById('description-input').value=splitted[4]
+function getExperience() {
+  handleResumee("personal-info", storePersonalInfo, addInfoInResume);
+  const splitted = handleResumee(
+    "experience",
+    storeExperience,
+    addExperienceInResumee
+  );
+  console.log({ splitted });
+  if (splitted !== undefined) {
+    document.getElementById("input-position").value = splitted[0];
+    document.getElementById("input-work").value = splitted[1];
+    document.getElementById("start-date").value = splitted[2];
+    document.getElementById("end-date").value = splitted[3];
+    document.getElementById("description-input").value = splitted[4];
+  }
 }
 
 function saveExperience() {
-    const inputPosition =document.getElementById('input-position').value
-    const inputWork = document.getElementById('input-work').value
-    const startDate = document.getElementById('start-date').value
-    const endDate = document.getElementById('end-date').value
-    const descriptionInput = document.getElementById('description-input').value
-    const arr = [inputPosition, inputWork,  startDate, endDate, descriptionInput]
-    addExperienceInResumee(arr)
-    sendToStorage(arr, addExperienceInResumee, 'experience')
+  const inputPosition = document.getElementById("input-position").value;
+  const inputWork = document.getElementById("input-work").value;
+  const startDate = document.getElementById("start-date").value;
+  const endDate = document.getElementById("end-date").value;
+  const descriptionInput = document.getElementById("description-input").value;
+  const arr = [inputPosition, inputWork, startDate, endDate, descriptionInput];
+  addExperienceInResumee(arr);
+  sendToStorage(arr, addExperienceInResumee, "experience");
 }
 
-function getEducation(){
-    handleResumee('personal-info', storePersonalInfo, addInfoInResume)
-    handleResumee('experience', storeExperience, addExperienceInResumee)
-    const splitted =  handleResumee('education', storeEducation, addEducationResumee)
-    document.getElementById('input-education').value = splitted[0];
+function getEducation() {
+  handleResumee("personal-info", storePersonalInfo, addInfoInResume);
+  handleResumee("experience", storeExperience, addExperienceInResumee);
+  const splitted = handleResumee(
+    "education",
+    storeEducation,
+    addEducationResumee
+  );
+  if (splitted !== undefined) {
+    document.getElementById("input-education").value = splitted[0];
     const e = document.getElementById("select-education");
     e.options[e.selectedIndex].text = splitted[1];
-    document.getElementById('input-education-graduate-date').value = splitted[2];
-    document.getElementById('input-education-description').value = splitted[3];
+    document.getElementById("input-education-graduate-date").value =
+      splitted[2];
+    document.getElementById("input-education-description").value = splitted[3];
+  }
 }
 
+function saveEducationInfo() {
+  const inputEducation = document.getElementById("input-education").value;
+  const inputEducationGraduateDate = document.getElementById(
+    "input-education-graduate-date"
+  ).value;
+  const inputEducationDescription = document.getElementById(
+    "input-education-description"
+  ).value;
 
-function saveEducationInfo(){
-    const inputEducation = document.getElementById('input-education').value;
-    const inputEducationGraduateDate = document.getElementById('input-education-graduate-date').value;
-    const inputEducationDescription = document.getElementById('input-education-description').value;
-
-    const e = document.getElementById("select-education");
-    const inputEducationDegree = e.options[e.selectedIndex].text;
-    // const inputEducationDegree = document.getElementById("select-education").options[selectedIndex].text;
-    const arr = [inputEducation, inputEducationDegree,  inputEducationGraduateDate, inputEducationDescription]
-    addEducationResumee(arr)
-    sendToStorage(arr, addEducationResumee, 'education')
+  const e = document.getElementById("select-education");
+  const inputEducationDegree = e.options[e.selectedIndex].text;
+  // const inputEducationDegree = document.getElementById("select-education").options[selectedIndex].text;
+  const arr = [
+    inputEducation,
+    inputEducationDegree,
+    inputEducationGraduateDate,
+    inputEducationDescription,
+  ];
+  addEducationResumee(arr);
+  sendToStorage(arr, addEducationResumee, "education");
 }
-
 
 // function displayImage() {
 //     let images = ""
@@ -185,22 +263,27 @@ function saveEducationInfo(){
 // }
 // let imagesArray = []
 
-window.addEventListener('load', function() {
-    console.log('oe')
-    document.querySelector('input[type="file"]').addEventListener('change', function() {
-        console.log({resumeImg})
+function imageUpload() {
+  window.addEventListener("load", function () {
+    document
+      .querySelector('input[type="file"]')
+      .addEventListener("change", function () {
+        // console.log({resumeImg})
 
         if (this.files && this.files[0]) {
-            console.log({resumeImg})
-            // var img = document.getElementById('resumeImg');
-            resumeImg.onload = () => {
-                URL.revokeObjectURL(resumeImg.src);  // no longer needed, free memory
-            }
-  
-            resumeImg.src = URL.createObjectURL(this.files[0]); // set src to blob url
+          console.log({ resumeImg });
+          // var img = document.getElementById('resumeImg');
+          resumeImg.onload = () => {
+            URL.revokeObjectURL(resumeImg.src); // no longer needed, free memory
+          };
+          resumeImg.src = URL.createObjectURL(this.files[0]); // set src to blob url
+          //   savePersonalInfo();
         }
-    });
+      });
   });
+}
+// imageUpload();
+
 // function uploadImg() {
 //     console.log('djcnksjdnckjsndckjs')
 //     const file = input.files
@@ -221,7 +304,7 @@ window.addEventListener('load', function() {
 //     document.getElementById('resume-end-date').innerHTML =  endDate;
 //     document.getElementById('resume-my-exp').innerHTML = descriptionInput;
 
-//     // additional info 
+//     // additional info
 
 //     const inputPositionAdd =document.getElementById('add-input-position').value
 //     const inputWorkAdd =document.getElementById('add-input-work').value
@@ -235,9 +318,7 @@ window.addEventListener('load', function() {
 //     document.getElementById('add-resume-end-date').innerHTML =  endDateAdd;
 //     document.getElementById('add-resume-my-exp').innerHTML = addDescription;
 
-    
 // }
-
 
 // saveInput();
 // secondPage();
---
+++ validate.js
@@ -0,0 +1,126 @@
+const namePerson = document.getElementById("input-name");
+const surname = document.getElementById("last-name");
+const email = document.getElementById("input-mail");
+const phoneNumber = document.getElementById("input-number");
+const aboutMe = document.getElementById("input-about-me");
+const position = document.getElementById("input-position");
+const employer = document.getElementById("input-work");
+const description = document.getElementById('description-input');
+const startDate = document.getElementById('start-date');
+const endDate =document.getElementById('end-date');
+const education =document.getElementById('input-education');
+const degree =document.getElementById('select-education');
+const graduateDate =document.getElementById('input-education-graduate-date');
+const graduationDescription =document.getElementById('input-education-description');
+
+const regGeo= /^[ა-ჰ]+$/;
+const regPhone= /^(\+?995)?(79\d{7}|5\d{8})$/;
+
+function checkAllInputs() {
+  checkNames(namePerson);
+  checkNames(surname);
+  checkAboutMe(aboutMe);
+  checkEmail(email)
+  checkPhone(phoneNumber)
+}
+
+function checkWorkInputs(){
+    checkWorkplace(employer)
+    checkWorkplace(position)
+    checkDescription(description)
+    checkDate(startDate)
+    checkDate(endDate)
+}
+
+function checkDate(target){
+    if( target.value === ""){
+        target.classList.add('input-not-valid')
+    } else{
+        target.classList.remove('input-not-valid')
+        target.classList.add('input-valid')
+    }
+
+}
+
+function checkSelect(){
+    const e = document.getElementById("select-education");
+    // e.options[e.selectedIndex].text;
+    console.log(  e.selectedIndex)
+    if( e.selectedIndex !== 0){
+        e.classList.remove('input-not-valid')
+        e.classList.add('input-valid')
+    }else{
+        e.classList.remove('input-valid')
+        e.classList.add('input-not-valid')
+    }
+}
+
+
+function onFocus(target) {
+  target.select();
+}
+
+function checkNames(target) {
+  if (
+    target.value.length < 2 ||
+    target.value === "" ||
+    !regGeo.test(target.value)
+  ) {
+    target.classList.remove("input-valid");
+    target.classList.add("input-not-valid");
+  } else {
+    target.classList.remove("input-not-valid");
+    target.classList.add("input-valid");
+  }
+}
+
+function checkWorkplace(target) {
+    if (
+      target.value.length < 2 ||
+      target.value === ""
+    ) {
+      target.classList.remove("input-valid");
+      target.classList.add("input-not-valid");
+    } else {
+      target.classList.remove("input-not-valid");
+      target.classList.add("input-valid");
+    }
+}
+
+function checkAboutMe(target) {
+  target.classList.add("input-valid");
+}
+
+function checkDescription(target) {
+    if( target.value === ""){
+        target.classList.add("input-not-valid");
+    } else{
+        target.classList.remove("input-not-valid");
+    target.classList.add("input-valid");
+    
+    }
+
+  }
+
+function checkEmail(target) {
+  if (
+    target.value !== "" &&
+    target.value.includes("@redberry.ge") &&
+    target.value.length > 12
+  ) {
+    target.classList.remove("input-not-valid");
+    target.classList.add("input-valid");
+  } else {
+    target.classList.remove("input-valid");
+    target.classList.add("input-not-valid");
+  }
+}
+function checkPhone(target) {
+  if (regPhone.test(target.value)) {
+    target.classList.remove("input-not-valid");
+    target.classList.add("input-valid");
+  } else {
+    target.classList.remove("input-valid");
+    target.classList.add("input-not-valid");
+  }
+}
